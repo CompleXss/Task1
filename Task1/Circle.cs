822290a0d386d37e1533e611a245ec1eb9bf4dc4
CompleXss <max.petrov.27@mail.ru>
2024-04-09 02:41:52 +0300

    Initial commit

---
+++ Task1/Circle.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1;
+
+public class Circle : Figure
+{
+	public double Radius { get; set; }
+
+	public Circle(double radius)
+	{
+		Radius = radius;
+	}
+
+
+
+	public override double Area()
+	{
+		return Math.PI * Radius * Radius;
+	}
+}
---
+++ Task1/Triangle.cs
@@ -0,0 +1,39 @@
+﻿namespace Task1;
+
+public class Triangle : Figure
+{
+	public double Side1 { get; set; }
+	public double Side2 { get; set; }
+	public double Side3 { get; set; }
+
+	public Triangle(double side1, double side2, double side3)
+	{
+		Side1 = side1;
+		Side2 = side2;
+		Side3 = side3;
+	}
+
+
+
+	public override double Area()
+	{
+		double halfPerimeter = (Side1 + Side2 + Side3) / 2;
+		double a = halfPerimeter - Side1;
+		double b = halfPerimeter - Side2;
+		double c = halfPerimeter - Side3;
+
+		return Math.Sqrt(halfPerimeter * a * b * c);
+	}
+
+	public bool IsRectangular()
+	{
+		double side1Squared = Side1 * Side1;
+		double side2Squared = Side2 * Side2;
+		double side3Squared = Side3 * Side3;
+
+		double sum = side1Squared + side2Squared + side3Squared;
+		double maxSide = Math.Max(Math.Max(side1Squared, side2Squared), side3Squared);
+
+		return maxSide == (sum - maxSide); // c^2 == a^2 + b^2
+	}
+}
